e3002c49b5eb32b9ccc240c56bbb8cff3cfc843b
Richard Hancock <me@richardhancock.co.uk>
2016-12-03 04:13:16 +0000

    Added descriptions to all content. Added all small portfolio images. Added 1 new header

--- gulpfile.js
+++ gulpfile.js
@@ -126,11 +126,10 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'image', 'minify-html', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['browserSync', 'less', 'minify-html', 'minify-css', 'minify-js'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
-    gulp.watch('img/**/*', ['image']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', ['minify-html', browserSync.reload]);
     gulp.watch('js/**/*.js', browserSync.reload);
