4aa7c616f6e351f6b3b1a4dd8a57a7431a3fa660
Richard Hancock <me@richardhancock.co.uk>
2017-06-17 05:36:15 +0100

    Improved Gulp Build Script. Closes #2
    Added a clean build option
    All files that are deployed onto the web server are now copied into dist.

--- gulpfile.js
+++ gulpfile.js
@@ -5,85 +5,56 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin')
+var htmlmin = require('gulp-htmlmin');
+var del = require('del');
 const imagemin = require('gulp-imagemin');
-var pkg = require('./package.json');
 
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
 
-// Compile LESS files from /less into /css
+// Compile LESS files from /less into dist/css
 gulp.task('less', function() {
-    return gulp.src('less/freelancer.less')
+    return gulp.src('less/*.less')
         .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+        .pipe(gulp.dest('dist/css'));
 });
 
 //Images
 gulp.task('image', function () {
    return gulp.src(["img/**/*", "!**/Thumbs.db", "!**/*.psd"])
        .pipe(imagemin())
-       .pipe(gulp.dest("dist/img"))
-       .pipe(browserSync.reload({
-           stream: true
-       }));
+       .pipe(gulp.dest("dist/img"));
 });
 
 //HTML
 gulp.task('minify-html', function() {
   return gulp.src('*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'))
-    .pipe(browserSync.reload({
-        stream: true
-    }));
+    .pipe(gulp.dest('dist'));
 });
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/freelancer.css')
+    return gulp.src(['!dist/css/*.min.css', 'dist/css/*.css'])
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+        .pipe(gulp.dest('dist/css'));
 });
 
 // Minify JS
 gulp.task('minify-js', function() {
-    gulp.src('js/freelancer.js')
-        .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
-
-    gulp.src(['!vendor/jquery-easing/*.min.js', 'vendor/jquery-easing/*.js'])
+	
+	gulp.src(['!vendor/jquery-easing/*.min.js', 'vendor/jquery-easing/*.js'])
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('vendor/jquery-easing'))
-
-     gulp.src(['!js/*.min.js', '!js/freelancer.js', "js/*.js"])
+	
+    gulp.src(['!js/*.min.js', "js/*.js"])
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+        .pipe(gulp.dest('dist/js'));
 });
 
 // Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
+gulp.task('pre-copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest('vendor/bootstrap'))
 
@@ -111,26 +82,27 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.json'
         ])
         .pipe(gulp.dest('vendor/font-awesome'))
-})
+});
+
+gulp.task('post-copy', ['pre-copy', 'less', 'minify-html', 'minify-css', 'minify-js'], function() {
+	gulp.src(['vendor/**/*'])
+		.pipe(gulp.dest('dist/vendor'));
+		
+	gulp.src(['favicons/**/*'])
+		.pipe(gulp.dest('dist/favicons'));
+	
+	gulp.src(['favicon.ico', 'Richard Hancock CV.pdf', '.htaccess', 'sitemap.xml.gz'])
+		.pipe(gulp.dest('dist'));
+	
+	gulp.src(['mail/**/*'])
+		.pipe(gulp.dest('dist/mail'));
+});
+
+gulp.task('clean', function() {
+	return del([
+		'dist/**/*'
+	]);
+});
 
 // Run everything
-gulp.task('default', ['copy', 'less', 'minify-html', 'minify-css', 'minify-js']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ''
-        },
-    })
-})
-
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-html', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', ['minify-html', browserSync.reload]);
-    gulp.watch('js/**/*.js', browserSync.reload);
-});
+gulp.task('default', ['pre-copy', 'less', 'minify-html', 'minify-css', 'minify-js', 'post-copy']);
